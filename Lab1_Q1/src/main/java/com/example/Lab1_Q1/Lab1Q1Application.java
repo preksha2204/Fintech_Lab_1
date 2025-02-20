c79a90560529b6d44a9c99a8d1eb35a571700180
preksha2204 <prekshapremnath@gmail.com>
2025-02-20 23:01:17 +0530

    Add files via upload

---
+++ Lab1_Q1/src/main/java/com/example/Lab1_Q1/Lab1Q1Application.java
@@ -0,0 +1,13 @@
+package com.example.Lab1_Q1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab1Q1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab1Q1Application.class, args);
+	}
+
+}
---
+++ Lab1_Q1/src/test/java/com/example/Lab1_Q1/Lab1Q1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Lab1_Q1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab1Q1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
